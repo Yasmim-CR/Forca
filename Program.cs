00ff3d3518e54658661fd5bbf1d283270a7d8168
miguelmm <miguelmm06@hotmail.com>
2025-03-29 20:42:49 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite uma palavra secreta:");
+string palavra = Console.ReadLine()!;
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine()!.ToLower();
+
+bool letraExiste = palavra.Contains(letra); 
+Console.WriteLine($"A letra {letra} existe na palavra secreta => {letraExiste}"); 
